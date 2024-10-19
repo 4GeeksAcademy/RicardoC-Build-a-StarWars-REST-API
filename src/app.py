bf9fce4fc187be785c538ce156cab95c907c1b99
RicardoClaro7 <167456148+RicardoClaro7@users.noreply.github.com>
2024-10-19 17:51:06 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People, FavoritePerson, FavoritePlanet
 #from models import Person
 
 app = Flask(__name__)
@@ -38,12 +38,284 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users=User.query.all() #Hacemos una consulta a la base de datos con .query y obtenemos todos los registros con .all
+    users_serialize=[] #Serializamos cada usuario de la BD y lo agregamos al nuestro array.
+    for user in users:
+        users_serialize.append(user.serialize())
+    return jsonify ({
+        'msg': 'Obtained Users!', #Devolvemos el array con los usuarios serializados y un status code ok(200).
+        'data': users_serialize}), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>/favorites', methods=['GET'])
+def get_favorites(id):
+#Hacemos una consulta en la BD para buscar los planetas favoritos que coincide con el id del usuario.
+    favorite_planets= FavoritePlanet.query.filter_by(user_id = id).all()
+    favorite_persons= FavoritePerson.query.filter_by(user_id = id).all()
+    if not favorite_planets and not favorite_persons: #Si no tenemos elementos agregados a favoritos devolvemos el siguiente mensaje:
+        return jsonify({'msg':"You don't have favorite items"}), 404
+    
+    favorite_planets_serialize=[] #Serializamos cada planeta de favoritePlanets mediante su relación(planet_relationship).
+    for planet_fav in favorite_planets:
+        favorite_planets_serialize.append(planet_fav.planet_relationship.serialize())
+    favorite_persons_serialize=[]
+    for people_fav in favorite_persons:
+        favorite_persons_serialize.append(people_fav.people_relationship.serialize())
+    #Retornamos los planetas favoritos ya serializados y el usuario que "le gustó" un planeta en específico.
+    return jsonify({
+        'msg':'ok',
+        'user_planet': favorite_planets[0].user_relationship.serialize(),
+        'user_people': favorite_persons[0].user_relationship.serialize(),
+        'favorite_planets': favorite_planets_serialize,
+        'favorite_persons': favorite_persons_serialize
+    }), 200 #Status code: ok.
 
-    return jsonify(response_body), 200
+@app.route('/peoples', methods=['GET'])
+def all_peoples():
+    peoples = People.query.all() #Obtenemos los registros de la tabla.
+    peoples_serialize = [] #Creamos un array para almacenarlos.
+    for person in peoples: #Almacenamos cada personaje en el array y los serializamos.
+        peoples_serialize.append(person.serialize())
+
+    return jsonify({
+        'msg':'Characters obtained',
+        'data': peoples_serialize
+    }), 200
+
+@app.route('/planets', methods=['GET'])
+def all_planets():
+    planets=Planet.query.all() #Obtenemos todos los planetas y almacenamos en un array.
+    planets_serialize=[]
+    for planet in planets: #agregamos cada planeta al array y lo serializamos.
+        planets_serialize.append(planet.serialize())
+        
+    return jsonify({
+        'msg':'Planets obtained',
+        'data': planets_serialize
+    }), 200
+
+@app.route('/peoples/<int:id>', methods=['GET'])
+def person(id):
+    person=People.query.get(id) #Buscamos un personaje por su id.
+    if not person: #Si no existe devolvemos el siguiente mensaje:
+        return jsonify({'msg':'Character not found.'}), 404
+    
+    return jsonify({ #Si existe lo serializamos(convertimos en formato json para que sea leido por el front) y retornamos.
+        'msg':'Character obtained',
+        'data': person.serialize()
+    }), 200
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def planet(id):
+    planet=Planet.query.get(id)
+    if not planet:
+        return jsonify({'msg':'Planet not found.'}), 404
+#Serializamos y agregamos al array los personajes que están en ese planeta, ya que hicimos una relación unidirecional entre planeta(Uno) y personajes(muchos).
+    inhabitants_serialize=[]
+    for habitant in planet.inhabitants: #Hay que iterar sobre el atributo inhabitants(personajes) del objeto Planet.
+        inhabitants_serialize.append(habitant.serialize())
+    data = planet.serialize()
+    data['inhabitants'] = inhabitants_serialize
+
+    return jsonify({
+        'msg':'Planet obtained',
+        'data': data
+    }), 200
+
+@app.route('/user', methods=['POST'])
+def add_user():
+    body=request.get_json(silent=True) #Obtenemos los datos del front.
+    if not body:
+        return jsonify({'msg':'Fields cannot be empty.'}),400 #Verificamos que el body no sea None(vacío).
+    
+    if 'name' not in body: #Verificamos la existencia de los campos en el body.
+        return jsonify({'msg':'The name field is required.'}), 400
+    
+    if 'email' not in body:
+        return jsonify({'msg':'The email field is required.'}), 400
+    
+    if 'password' not in body:
+        return jsonify({'msg': 'The password field is required.'}), 400
+    
+    email = User.query.filter_by(email=body['email']).first() #Verificamos que el email no exista en la BD.
+    if email:
+        return jsonify({'msg':'This email is taken; please choose another.'}), 400
+    
+    user=User() #Instanciamos nuevo usuario.
+    user.name=body['name'] #Asignamos los campos del body a los atributos del objeto user(tabla).
+    user.email=body['email']
+    user.password=body['password']
+    user.is_active=True #Asignamos un valor por defecto ya que este campo no es null en la tabla.
+    db.session.add(user)#Añadimos la instancia.
+    db.session.commit()#Guardamos
+
+    return jsonify({ #serializamos usuario(para ver y usar esa info y retornamos).
+        'msg':'User created',
+        'data': user.serialize()
+    }), 201
+
+@app.route('/planet', methods=['POST'])
+def add_planet():
+    body=request.get_json(silent=True) #Obtenemos los datos del front.
+    if not body:
+        return jsonify({'msg':'Fields cannot be empty.'}),400 #Verificamos que el body no sea None(vacío).
+    
+    if 'name' not in body: #Verificamos la existencia de los campos en el body.
+        return jsonify({'msg':'The name field is required.'}), 400
+    
+    if 'population' not in body:
+        return jsonify({'msg':'The population field is required.'}), 400
+    
+    if 'diameter' not in body:
+        return jsonify({'msg': 'The diameter field is required.'}), 400
+    
+    if 'climated' not in body:
+        return jsonify({'msg':'The climated field is required.'}), 400
+    
+    if 'terrain' not in body:
+        return jsonify({'msg': 'The terrain field is required.'}), 400
+    
+    planet_name = Planet.query.filter_by(name=body['name']).first() #Verificamos que el name no exista en la BD.
+    if planet_name:
+        return jsonify({'msg':'This name is taken; please choose another.'}), 400
+    
+    planet=Planet() #Instanciamos nuevo planeta.
+    planet.name=body['name'] #Asignamos los campos del body a los atributos del objeto planet(tabla).
+    planet.population=body['population']
+    planet.diameter=body['diameter']
+    planet.climated=body['climated']
+    planet.terrain=body['terrain']
+    db.session.add(planet)#Añadimos la instancia.
+    db.session.commit()#Guardamos
+
+    return jsonify({ #serializamos usuario(para ver y usar esa info y retornamos).
+        'msg':'Planet created',
+        'data': planet.serialize()
+    }), 201
+
+@app.route('/people', methods=['POST'])
+def add_person():
+    body=request.get_json(silent=True) #Obtenemos los datos del front.
+    if not body:
+        return jsonify({'msg':'Fields cannot be empty.'}),400 #Verificamos que el body no sea None(vacío).
+    
+    if 'name' not in body: #Verificamos la existencia de los campos en el body.
+        return jsonify({'msg':'The name field is required.'}), 400
+    
+    if 'specie' not in body:
+        return jsonify({'msg':'The specie field is required.'}), 400
+    
+    if 'gender' not in body:
+        return jsonify({'msg': 'The gender field is required.'}), 400
+    
+    if 'height' not in body:
+        return jsonify({'msg':'The height field is required.'}), 400
+    
+    if 'weight' not in body:
+        return jsonify({'msg': 'The weight field is required.'}), 400
+    
+    if 'age' not in body:
+        return jsonify({'msg': 'The age field is required.'}), 400
+    
+    person_name = People.query.filter_by(name=body['name']).first() #Verificamos que el name no exista en la BD.
+    if person_name:
+        return jsonify({'msg':'This name is taken; please choose another.'}), 400
+    
+    person=People() #Instanciamos nueva persona.
+    person.name=body['name'] #Asignamos los campos del body a los atributos del objeto People(tabla).
+    person.specie=body['specie']
+    person.gender=body['gender']
+    person.height=body['height']
+    person.weight=body['weight']
+    person.age=body['age']
+    db.session.add(person)#Añadimos la instancia.
+    db.session.commit()#Guardamos
+
+    return jsonify({ #serializamos usuario(para ver y usar esa info y retornamos).
+        'msg':'Person created',
+        'data': person.serialize()
+    }), 201
+
+@app.route('/favorite/planet/<int:planet_id>/<int:user_id>', methods=['POST'])
+def add_favorite_planet(user_id,planet_id):
+    user=User.query.get(user_id)
+    if not user:
+        return jsonify({'msg': 'User Not Found'}),404
+    
+    planet=Planet.query.get(planet_id) #Obtenemos un registro especifico de la tabla.
+    if not planet:
+        return jsonify({'msg':'Planet Not Found'}),404
+    
+#Verificamos que el id del usuario y del planeta coincida con los actuales y obtenemos el primer registro.
+    favorite_planet = FavoritePlanet.query.filter_by(user_id = user_id, planet_id = planet_id).first()
+    if favorite_planet:
+        return jsonify({'msg':f'The planet {planet.name} is already added to favorites'}), 400
+    
+    favorite = FavoritePlanet() #Instanciamos el nuevo favorito.
+    favorite.user_id = user_id #Asignamos el id del usuario al campo user_id del objeto(tabla).
+    favorite.planet_id = planet_id
+    db.session.add(favorite) #Añadimos la instancia.
+    db.session.commit() #Guardamos
+    return jsonify({'msg': 'Planet bookmarked'}), 200
+
+@app.route('/favorite/person/<int:people_id>/<int:user_id>', methods=['POST'])
+def add_favorite_person(user_id,people_id):
+    user=User.query.get(user_id)
+    if not user:
+        return jsonify({'msg': 'User Not Found'}),404
+    
+    person=People.query.get(people_id)
+    if not person:
+        return jsonify({'msg':'Person Not Found'}),404
+    
+    favorite_person = FavoritePerson.query.filter_by(user_id = user_id, people_id = people_id).first()
+    if favorite_person:
+        return jsonify({'msg':f'The person {person.name} is already added to favorites'}), 400
+    
+    favorite = FavoritePerson()
+    favorite.user_id = user_id
+    favorite.people_id = people_id
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify({'msg': 'Person bookmarked'}), 200
+
+@app.route('/favorite/planet/<int:planet_id>/<int:user_id>', methods=['DELETE'])
+def delete_favorite_planet(user_id,planet_id):
+    user=User.query.get(user_id)
+    if not user:
+        return jsonify({'msg': 'User Not Found'}),404
+    
+    planet=Planet.query.get(planet_id) #Obtenemos el registro especifico.
+    if not planet:
+        return jsonify({'msg':'Planet Not Found'}),404
+    
+#Verificamos que el favorito no exista ya en la lista.
+    favorite_planet = FavoritePlanet.query.filter_by(user_id = user_id, planet_id = planet_id).first()
+    if not favorite_planet:
+        return jsonify({'msg':'The planet is not in favorites'}), 404
+    
+    db.session.delete(favorite_planet) #Eliminamos el favorito.
+    db.session.commit() #Guardamos.
+    return jsonify({'msg': f'Planet {planet.name} removed from watch list'}), 200 #f para colocar atributos
+
+@app.route('/favorite/person/<int:people_id>/<int:user_id>', methods=['DELETE'])
+def delete_favorite_person(user_id,people_id):
+    user=User.query.get(user_id) #get solo para PK(id)
+    if not user:
+        return jsonify({'msg': 'User Not Found'}),404
+    
+    person=People.query.get(people_id)
+    if not person:
+        return jsonify({'msg':'Person Not Found'}),404
+    
+    favorite_person = FavoritePerson.query.filter_by(user_id = user_id, people_id = people_id).first()
+    if not favorite_person:
+        return jsonify({'msg':'The person is not in favorites'}), 404
+    
+    db.session.add(favorite_person)
+    db.session.commit()
+    
+    return jsonify({'msg': f'Person {person.name} removed from watch list'}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,134 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(25), nullable=False )
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorite_planets=db.relationship('FavoritePlanet', back_populates='user_relationship')
+    favorite_persons=db.relationship('FavoritePerson', back_populates='user_relationship')
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
+    def __repr__(self): #Sirve para imprimir en la terminal de Postgresql.
+        return '<User %r>' % self.name
+    
+    def serialize(self): #Sirve para transformar la info en diccionarios python.
         return {
             "id": self.id,
-            "email": self.email,
+            'name': self.name,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
+class Planet(db.Model):
+    __tablename__='planets'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20), unique=True, nullable=False)
+    population=db.Column(db.Integer, nullable=False)
+    diameter=db.Column(db.Integer, nullable=False)
+    climated=db.Column(db.String(15), nullable=False)
+    terrain=db.Column(db.String(15), nullable=False)
+    favorite_by=db.relationship('FavoritePlanet', back_populates='planet_relationship')#Relación bidireccional con FavoritePlanet
+    inhabitants=db.relationship('People', back_populates='planet_relationship')
+
+    def __repr__(self):
+        return f'<Planet {self.name}>'
+    
+    def serialize(self):
+        return{
+            'id':self.id,
+            'name':self.name,
+            'population':self.population,
+            'diameter':self.diameter,
+            'climated':self.climated,
+            'terrain':self.terrain
+        }
+    
+class People(db.Model):
+    __tablename__='peoples'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20),unique=True, nullable=False)
+    specie=db.Column(db.String(15), nullable=False)
+    gender=db.Column(db.String(15), nullable=False)
+    height=db.Column(db.Integer, nullable=False)
+    weight=db.Column(db.Integer, nullable=False)
+    age=db.Column(db.Integer, nullable=False)
+    favorite_by=db.relationship('FavoritePerson', back_populates='people_relationship')
+    planet_id=db.Column(db.Integer, db.ForeignKey('planets.id'))#Relación uno(Planet) a muchos(People)."Un planeta tiene muchos personajes".
+    planet_relationship=db.relationship(Planet) #Relación unidireccional(ya que desde planetas puedo acceder a personajes pero no visceversa).
+    #Ya que el back_populates lo defino solamente en Planet. Fk en relación uno a muchos siempre va en el lado muchos(People).
+    
+    def __repr__(self):
+        return f'<People {self.name}>'
+    
+    def serialize(self):
+        return{
+            'id':self.id,
+            'name':self.name,
+            'specie':self.specie,
+            'gender':self.gender,
+            'height':self.height,
+            'weight':self.weight,
+            'age':self.age
+        }
+    
+class FavoritePlanet(db.Model): #Tabla de unión que relaciona usuario con planets(Muchos a Muchos).
+    __tablename__='favoritePlanets'
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id')) #Llave foránea que apunta al id de la tabla user.
+    user_relationship=db.relationship(User, back_populates='favorite_planets') #Relación que apunta a la clase User y a la variable que contiene la relación.
+    planet_id=db.Column(db.Integer, db.ForeignKey('planets.id'))
+    planet_relationship=db.relationship(Planet, back_populates='favorite_by')
+
+    def __repr__(self):
+        return f'<User {self.user_id} liked the planet{self.planet_id}>'
+    
+    def serialize(self):
+        return{
+            'id':self.id,
+            'user_id':self.user_id,
+            'planet_id':self.planet_id
+        }
+
+class FavoritePerson(db.Model):
+    __tablename__='favoritePersons'
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_relationship=db.relationship(User, back_populates='favorite_persons')
+    people_id=db.Column(db.Integer, db.ForeignKey('peoples.id'))
+    people_relationship=db.relationship(People, back_populates='favorite_by')
+
+    def __repr__(self):
+        return f'<User {self.user_id} liked the person {self.people_id}>'
+    
+    def serialize(self):
+        return{
+            'id':self.id,
+            'user_id':self.user_id,
+            'people_id':self.people_id
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
